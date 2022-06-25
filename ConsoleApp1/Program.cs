7fdbf65efd1f4a1a74bc6d65818983736dca013c
X_99 <X_99@X99>
2022-06-25 12:33:54 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo !!!");
+            Console.ReadLine(); // Para la ejecucion hasta presionar tecla
+        }
+    }
+}
